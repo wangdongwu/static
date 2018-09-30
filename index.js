fb89747df39cab301831b6f5c2b7c3a62b2e5c1b
王东武 <496414500@qq.com>
2018-09-30 17:06:21 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+console.log('**************runing**************')
+console.log('**************runing2asd2222**************')
+console.log('**************runing33333**************')
+console.log('**************runing444asd44**************')
+console.log('**************runinasdg55555**************')
+console.log('**************runing66666**************')
+console.log('**************runing7asdasd7777**************')
+console.log('**************runing88888**************')
+console.log('**************runing999asd**************')
+_.set({},'a','aaas')
